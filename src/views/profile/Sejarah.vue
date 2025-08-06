304232f1314a35ae52820e4a47a2e88a911213c6
alghozii_zyy <shobarudin67@students.amikom.ac.id>
2025-08-07 00:56:48 +0700

    update sejarah

--- src/views/profile/Sejarah.vue
+++ src/views/profile/Sejarah.vue
@@ -1,25 +1,31 @@
 <script setup>
-import { useStore } from 'vuex';
-import { computed } from 'vue';
+import { useStore } from "vuex";
+import { computed } from "vue";
 
 const store = useStore();
-const deskripsi = computed(() => store.getters.getProfileContent.sejarah.sejarahDeskripsi);
+const deskripsi = computed(
+  () => store.getters.getProfileContent.sejarah.sejarahDeskripsi
+);
 </script>
 
 <template>
   <div>
     <!-- Gambar Header -->
-    <div
-      class="relative w-full h-[750px] md:h-[750px] bg-cover bg-center flex items-center justify-center text-white text-center"
-      style="background-image: url('/background/bg-sejarah.png')"
-    >
-      <div class="absolute"></div>
-      <div class="relative z-10 px-6">
+    <div class="relative w-full h-[750px] overflow-hidden">
+      <img
+        src="/background/bg-sejarah.png"
+        class="absolute top-0 left-0 w-full h-full object-cover object-center z-0"
+        alt="Background"
+      />
+      <div
+        class="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-6"
+      >
         <h1 class="text-3xl md:text-5xl font-bold mb-4">
           SEJARAH DAN PERKEMBANGAN MAN 4 BANTUL
         </h1>
         <p class="max-w-3xl mx-auto text-sm md:text-lg">
-          Pada Oktober 1969, berdiri Pendidikan Guru Agama Latihan (PGAL) Fakultas Tarbiyah IAIN Sunan Kalijaga Yogyakarta berdasarkan SK Dekan No. 4/D/T/1969 tanggal 14 Januari 1969...
+          Pada Oktober 1969, berdiri Pendidikan Guru Agama Latihan (PGAL)
+          Fakultas Tarbiyah...
         </p>
       </div>
     </div>
@@ -27,13 +33,20 @@ const deskripsi = computed(() => store.getters.getProfileContent.sejarah.sejarah
     <!-- Garis Tokoh -->
     <section class="px-6 py-12 text-center">
       <h2 class="text-lg font-semibold mb-6 mt-4">
-        Madrasah Aliyah Negeri Lab UIN Yogyakarta adalah buah dari rentetan panjang perjuangan yang diprakarsai oleh :
+        Madrasah Aliyah Negeri Lab UIN Yogyakarta adalah buah dari rentetan
+        panjang perjuangan yang diprakarsai oleh :
       </h2>
-      <img src="/background/sejarah.png" alt="Tokoh Sejarah" class="mx-auto mt-12" />
+      <img
+        src="/background/sejarah.png"
+        alt="Tokoh Sejarah"
+        class="mx-auto mt-12"
+      />
     </section>
 
     <!-- Deskripsi Panjang -->
-    <section class="px-6 md:px-16 py-10 max-w-5xl mx-auto text-justify text-gray-800 leading-relaxed">
+    <section
+      class="px-6 md:px-16 py-10 max-w-5xl mx-auto text-justify text-gray-800 leading-relaxed"
+    >
       <p v-html="deskripsi"></p>
     </section>
   </div>
